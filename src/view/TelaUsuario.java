52f9c115f8ac424437008c255a58ee6d60261acc
Anthony <anthony.oliveira5@hotmail.com>
2024-05-21 18:34:49 -0300

    Adiciona o usuário selecionado ao alterar

--- src/view/TelaUsuario.java
+++ src/view/TelaUsuario.java
@@ -12,6 +12,8 @@ import java.awt.ComponentOrientation;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.SpringLayout;
+import javax.swing.SwingUtilities;
+
 import java.awt.Point;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -34,6 +36,7 @@ import javax.swing.JOptionPane;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.JDesktopPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -42,6 +45,7 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import java.awt.Cursor;
 import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
 
 
 public class TelaUsuario extends JInternalFrame {
@@ -53,8 +57,12 @@ public class TelaUsuario extends JInternalFrame {
 	private static final long serialVersionUID = 1L;
 	private JTextField txtNomeUsuario;
 	private JTextField txtSalarioUsuario;
-	private JTextField txtUserUsuario;
+	private JTextField txtEmailUsuario;
 	private JTextField txtSenhaUsuario;
+	private JComboBox cbPerfilUsuario = new JComboBox();
+	private JFormattedTextField ftxtCpfUsuario = new JFormattedTextField();
+	private JFormattedTextField ftxtDataNascimentoUsuario = new JFormattedTextField();
+	private JComboBox cbStatusUsuario = new JComboBox();
 
 	/**
 	 * Launch the application.
@@ -123,10 +131,10 @@ public class TelaUsuario extends JInternalFrame {
 		getContentPane().add(lblEmail);
 		lblEmail.setFont(new Font("Arial Black", Font.PLAIN, 14));
 			
-		txtUserUsuario = new JTextField();
-		txtUserUsuario.setColumns(10);
-		txtUserUsuario.setBounds(261, 220, 176, 20);
-		getContentPane().add(txtUserUsuario);
+		txtEmailUsuario = new JTextField();
+		txtEmailUsuario.setColumns(10);
+		txtEmailUsuario.setBounds(261, 220, 176, 20);
+		getContentPane().add(txtEmailUsuario);
 		
 		JLabel lblSenhaUsuario = new JLabel("Senha");
 		lblSenhaUsuario.setFont(new Font("Arial Black", Font.PLAIN, 14));
@@ -143,7 +151,6 @@ public class TelaUsuario extends JInternalFrame {
 		lblPerfilUsuario.setBounds(202, 283, 49, 21);
 		getContentPane().add(lblPerfilUsuario);
 		
-		final JComboBox cbPerfilUsuario = new JComboBox();
 		cbPerfilUsuario.setModel(new DefaultComboBoxModel(new String[] {"Administrador", "Funcionário"}));
 		cbPerfilUsuario.setBounds(261, 284, 176, 22);
 		getContentPane().add(cbPerfilUsuario);
@@ -157,27 +164,21 @@ public class TelaUsuario extends JInternalFrame {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		final JFormattedTextField ftxtCpfUsuario = new JFormattedTextField(cpfMask);
+		ftxtCpfUsuario = new JFormattedTextField(cpfMask);
 		ftxtCpfUsuario.setText("");
 		ftxtCpfUsuario.setBounds(261, 119, 176, 20);
 		getContentPane().add(ftxtCpfUsuario);
 		
-		final JFormattedTextField ftxtDataNascimentoUsuario = new JFormattedTextField(dataMask);
+		ftxtDataNascimentoUsuario = new JFormattedTextField(dataMask);
 		ftxtDataNascimentoUsuario.setText("");
 		ftxtDataNascimentoUsuario.setBounds(261, 150, 176, 20);
 		getContentPane().add(ftxtDataNascimentoUsuario);
-		
-		JLabel lblConsultarUsuario = new JLabel("ConsultarUsuario");
-		lblConsultarUsuario.setFont(new Font("Arial Black", Font.PLAIN, 14));
-		lblConsultarUsuario.setBounds(469, 86, 161, 21);
-		getContentPane().add(lblConsultarUsuario);
 		
 		JLabel lblStatusUsuario = new JLabel("Status");
 		lblStatusUsuario.setFont(new Font("Arial Black", Font.PLAIN, 14));
 		lblStatusUsuario.setBounds(202, 312, 49, 21);
 		getContentPane().add(lblStatusUsuario);
 		
-		JComboBox cbStatusUsuario = new JComboBox();
 		cbStatusUsuario.setModel(new DefaultComboBoxModel(new String[] {"Ativo", "Inativo"}));
 		cbStatusUsuario.setBounds(261, 313, 176, 22);
 		getContentPane().add(cbStatusUsuario);
@@ -187,15 +188,17 @@ public class TelaUsuario extends JInternalFrame {
 		btnCadastrarUsuario.setBackground(new Color(240, 240, 240));
 		btnCadastrarUsuario.setIcon(new ImageIcon(TelaUsuario.class.getResource("/icones/addicon.png")));
 		btnCadastrarUsuario.setPreferredSize(new Dimension(80, 80));
-		btnCadastrarUsuario.setBounds(507, 374, 117, 68);
+		btnCadastrarUsuario.setBounds(469, 374, 155, 68);
 		getContentPane().add(btnCadastrarUsuario);
 		btnCadastrarUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				double salarioUsuario = Integer.parseInt(txtSalarioUsuario.getText());
+				double salarioUsuario = Double.parseDouble(txtSalarioUsuario.getText());
 				UsuarioController usuarioController = new UsuarioController();
 				try {
 					System.out.println(ftxtDataNascimentoUsuario.getText());
-					usuarioController.cadastrarUsuario(txtNomeUsuario.getText(),ftxtCpfUsuario.getText(),ftxtDataNascimentoUsuario.getText(),salarioUsuario,txtUserUsuario.getText(),txtSenhaUsuario.getText(),cbPerfilUsuario.getSelectedItem().toString(),cbStatusUsuario.getSelectedItem().toString());
+					if(usuarioSelecionado == null) {
+						usuarioController.cadastrarUsuario(txtNomeUsuario.getText(),ftxtCpfUsuario.getText(),ftxtDataNascimentoUsuario.getText(), salarioUsuario,txtEmailUsuario.getText(),txtSenhaUsuario.getText(),cbPerfilUsuario.getSelectedItem().toString(),cbStatusUsuario.getSelectedItem().toString());
+					}
 				} catch (ParseException | ExceptionDao e1) {
 					JOptionPane.showMessageDialog(null, e1 + "Não foi possível converter os dados captados");
 					e1.printStackTrace();
@@ -203,16 +206,27 @@ public class TelaUsuario extends JInternalFrame {
 			}
 		});
 		
-		JButton btnConsultarUsuario = new JButton("");
+		
+		JButton btnConsultarUsuario = new JButton("Consultar");
 		btnConsultarUsuario.setIcon(new ImageIcon(TelaUsuario.class.getResource("/icones/findicon.png")));
-		btnConsultarUsuario.setBounds(507, 11, 117, 68);
+		btnConsultarUsuario.setBounds(469, 11, 155, 68);
 		getContentPane().add(btnConsultarUsuario);
 		btnConsultarUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				TelaConsultaUsuario listarUsuarios = new TelaConsultaUsuario();
+				JDesktopPane desktop = getDesktopPane();
+				JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(desktop);
+				if (frame instanceof TelaMenuPrincipal) {
+					JInternalFrame[] frames = desktop.getAllFrames();
+	                for (JInternalFrame frame1 : frames) {
+	                    frame1.dispose();
+	                }
+				}
+				desktop.add(listarUsuarios);
+				listarUsuarios.setVisible(true);
 			}
 		});
-		
+			
 		
 		setIconifiable(true);
 		setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
@@ -226,6 +240,20 @@ public class TelaUsuario extends JInternalFrame {
 		setAlignmentY(Component.TOP_ALIGNMENT);
 		setAlignmentX(Component.LEFT_ALIGNMENT);
 		setBounds(100, 100, 640, 480);
-
+		
+		if(usuarioSelecionado!=null) {
+			preencherCampos(usuarioSelecionado);
+		}
+	}
+	
+	private void preencherCampos(Usuario usuarioSelecionado) {
+		txtNomeUsuario.setText(usuarioSelecionado.getNomeUsuario());
+		ftxtCpfUsuario.setText(usuarioSelecionado.getCpfUsuario());
+		ftxtDataNascimentoUsuario.setValue(usuarioSelecionado.getDataNascimentoUsuario());
+		txtEmailUsuario.setText(usuarioSelecionado.getEmailUsuario());
+		//txtSalarioUsuario.setValue(usuarioSelecionado.getSalarioUsuario());
+		cbPerfilUsuario.setSelectedItem(usuarioSelecionado.getPerfilUsuario());
+		cbStatusUsuario.setSelectedItem(usuarioSelecionado.getStatusUsuario());
+		
 	}
 }
